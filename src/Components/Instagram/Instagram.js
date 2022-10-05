5b9d3e3486b376a041a6d4d211da54798c01d5ad
Shinnosuke-Takahashi <shinnosuketakahashisemail@gmail.com>
2022-10-05 13:14:43 -0400

    ig widget style fix

--- src/Components/Instagram/Instagram.js
+++ src/Components/Instagram/Instagram.js
@@ -9,7 +9,7 @@ const Instagram = () => (
         <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
         <iframe src="https://cdn.lightwidget.com/widgets/c80817fbe73c532facdb01467d68e01e.html" 
         scrolling="no" allowtransparency="true" 
-        class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+        class="lightwidget-widget"></iframe>
         </div>
     </Parallax>
 );
